cc154b9a5006d4dd7bb7c7f5510ad7136fd2da4b
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-05-19 16:19:43 +0200

    created spring boot initial project

---
+++ src/main/java/com/example/propertyrental/PropertyRental.java
@@ -0,0 +1,13 @@
+package com.example.propertyrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyRental {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PropertyRental.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/propertyrental/PropertyRentalTests.java
@@ -0,0 +1,13 @@
+package com.example.propertyrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyRentalTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
