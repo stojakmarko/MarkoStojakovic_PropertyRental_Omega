54dc5a702ca71ccd37741c3bb7a70a4380a44d6b
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-05-23 15:13:49 +0200

    added files for security config

---
+++ src/main/java/com/example/propertyrental/security/CustemDetailUserService.java
@@ -0,0 +1,24 @@
+package com.example.propertyrental.security;
+
+
+import com.example.propertyrental.model.User;
+import com.example.propertyrental.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustemDetailUserService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findUserByUserName(username);
+        CustemUserDetails custemUserDetails = new CustemUserDetails(user);
+        return  custemUserDetails;
+    }
+}
