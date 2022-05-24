ec2995d6998a50f35896163b792ac9e57afb57bd
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-05-24 16:06:54 +0200

    create UserService

---
+++ src/main/java/com/example/propertyrental/service/UserService.java
@@ -0,0 +1,30 @@
+package com.example.propertyrental.service;
+
+import com.example.propertyrental.dto.RegistrationRequestDto;
+import com.example.propertyrental.mapper.RegistrationUserMapper;
+import com.example.propertyrental.model.User;
+import com.example.propertyrental.model.UserRole;
+import com.example.propertyrental.repository.UserRepository;
+import com.example.propertyrental.repository.UserRoleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private UserRepository userRepository;
+    private UserRoleRepository userRoleRepository;
+    private RegistrationUserMapper registrationUserMapper;
+
+
+    public void createClient(RegistrationRequestDto registrationRequest){
+        UserRole userRole = userRoleRepository.findUserRoleByRole("CLIENT");
+        User user = registrationUserMapper.toUser(registrationRequest);
+        user.setUserRole(userRole);
+        userRepository.save(user);
+
+
+    }
+
+}
