ca65e4b2566dd17a02f080e98b30e0ba362c8027
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-05-24 13:36:31 +0200

    added controller for authentication and dto

---
+++ src/main/java/com/example/propertyrental/controllers/AuthenticationController.java
@@ -0,0 +1,49 @@
+package com.example.propertyrental.controllers;
+
+
+import com.example.propertyrental.dto.AuthenticateRequest;
+import com.example.propertyrental.dto.AuthenticationResponse;
+import com.example.propertyrental.security.CustemDetailUserService;
+import com.example.propertyrental.security.CustemUserDetails;
+import com.example.propertyrental.security.JwtTokenUtil;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sign")
+@AllArgsConstructor
+public class AuthenticationController {
+
+    private JwtTokenUtil jwt;
+    private AuthenticationManager authenticationManager;
+    private CustemDetailUserService userService;
+
+
+    @PostMapping
+    public ResponseEntity<?> authentication(@RequestBody AuthenticateRequest authenticateRequest){
+
+        try {
+
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authenticateRequest.username(),authenticateRequest.password()));
+            CustemUserDetails userDetails =(CustemUserDetails)userService.loadUserByUsername(authenticateRequest.username());
+            String token = jwt.genarateToken(userDetails.getUser());
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization",token);
+            return new ResponseEntity<String>(headers, HttpStatus.OK);
+
+        }catch (Exception e){
+            AuthenticationResponse authenticationResponse = new AuthenticationResponse("Bad credentials");
+            return new ResponseEntity<AuthenticationResponse>(authenticationResponse, HttpStatus.BAD_REQUEST);
+
+        }
+
+    }
+}
---
+++ src/main/java/com/example/propertyrental/dto/AuthenticationResponse.java
@@ -0,0 +1,4 @@
+package com.example.propertyrental.dto;
+
+public record AuthenticationResponse(String message) {
+}
