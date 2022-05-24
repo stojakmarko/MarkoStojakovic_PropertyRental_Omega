880ddaa592c57a1d9b21afabe966197dd4640d3a
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-05-24 15:57:12 +0200

    create method findByRole in UserRoleRepo

--- src/main/java/com/example/propertyrental/repository/UserRoleRepository.java
+++ src/main/java/com/example/propertyrental/repository/UserRoleRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole,Long> {
+    UserRole findUserRoleByRole(String role);
 }
