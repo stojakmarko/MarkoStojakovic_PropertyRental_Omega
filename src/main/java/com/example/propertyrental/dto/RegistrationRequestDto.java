a646f1a68424316f4129bb7e87ac20968707b5ca
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-05-26 09:58:41 +0200

    added annotations for validation

--- src/main/java/com/example/propertyrental/dto/RegistrationRequestDto.java
+++ src/main/java/com/example/propertyrental/dto/RegistrationRequestDto.java
@@ -1,24 +1,37 @@
 package com.example.propertyrental.dto;
 
-import lombok.*;
+import com.example.propertyrental.validation.FieldsValueMatch;
+import com.example.propertyrental.validation.UniqueEmail;
+import com.example.propertyrental.validation.UniqueUsername;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@FieldsValueMatch.List({
+        @FieldsValueMatch(
+                field = "password",
+                fieldMatch = "verifyPassword",
+                message = "Passwords do not match!"
+        )
+})
 public class RegistrationRequestDto {
 
 
-    @Size(min = 3,max = 50,message = "Firstname should have minimum 3 and maximum 50 characters ")
+    @Size(min = 3, max = 50, message = "Firstname should have minimum 3 and maximum 50 characters ")
     private String firstname;
-    @Size(min = 3,max = 50,message = "Lastname should have minimum 3 and maximum 50 characters ")
+    @Size(min = 3, max = 50, message = "Lastname should have minimum 3 and maximum 50 characters ")
     private String lastname;
-    //to do --custom validation for username
+    @UniqueUsername
     private String username;
-    //to do-- validator are password equal
     private String password;
-    private String verifypassword;
-    @Email
+    private String verifyPassword;
+    @Email(message = "Email should be farmatted")
+    @UniqueEmail
     private String email;
 }
