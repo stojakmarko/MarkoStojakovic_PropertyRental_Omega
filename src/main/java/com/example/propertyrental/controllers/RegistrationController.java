01a2b111138ea6317e05ddcce12645e764c5b577
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-05-26 09:57:19 +0200

    added valid annotation

--- src/main/java/com/example/propertyrental/controllers/RegistrationController.java
+++ src/main/java/com/example/propertyrental/controllers/RegistrationController.java
@@ -4,14 +4,14 @@ import com.example.propertyrental.dto.RegistrationRequestDto;
 import com.example.propertyrental.dto.UserDto;
 import com.example.propertyrental.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/registration")
@@ -21,10 +21,9 @@ public class RegistrationController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserDto> userRegistration(@RequestBody RegistrationRequestDto registrationRequest) {
+    public ResponseEntity<UserDto> userRegistration(@Valid @RequestBody RegistrationRequestDto registrationRequest) {
         UserDto userDto = userService.createClient(registrationRequest);
-        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/registration").toUriString());
-        return ResponseEntity.created(uri).body(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userDto);
     }
 
 
