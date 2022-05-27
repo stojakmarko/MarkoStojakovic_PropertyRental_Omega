29528c7401fdc7ebee1dfcac192ee38136402020
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-05-27 10:55:44 +0200

    add property mapper

---
+++ src/main/java/com/example/propertyrental/mapper/PropertyMapper.java
@@ -0,0 +1,44 @@
+package com.example.propertyrental.mapper;
+
+import com.example.propertyrental.dto.PropertyRequestDto;
+import com.example.propertyrental.dto.PropertyResponseDto;
+import com.example.propertyrental.model.Property;
+import com.example.propertyrental.model.User;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class PropertyMapper {
+
+
+    public PropertyResponseDto toPropertyResponseDto(Property property) {
+        return PropertyResponseDto.builder()
+                .id(property.getId())
+                .name(property.getName())
+                .location(property.getLocation())
+                .price(property.getPrice())
+                .availability(property.isAvailability())
+                .freeParking(property.isFreeParking())
+                .numOfBedrooms(property.getNumOfBedrooms())
+                .numOfSleepPlace(property.getNumOfSleepPlace())
+                .pool(property.isPool())
+                .wifi(property.isWifi())
+                .build();
+    }
+
+    public Property toProperty(PropertyRequestDto propertyRequestDto, User owner) {
+        return Property.builder()
+                .name(propertyRequestDto.getName())
+                .location(propertyRequestDto.getLocation())
+                .price(propertyRequestDto.getPrice())
+                .availability(propertyRequestDto.isAvailability())
+                .freeParking(propertyRequestDto.isFreeParking())
+                .numOfBedrooms(propertyRequestDto.getNumOfBedrooms())
+                .numOfSleepPlace(propertyRequestDto.getNumOfSleepPlace())
+                .pool(propertyRequestDto.isPool())
+                .wifi(propertyRequestDto.isWifi())
+                .owner(owner)
+                .build();
+    }
+}
