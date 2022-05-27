d7382f68c3c0ccc7a9eb4a40242261fad21e0715
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-05-27 10:56:29 +0200

    add property service

---
+++ src/main/java/com/example/propertyrental/service/PropertyService.java
@@ -0,0 +1,55 @@
+package com.example.propertyrental.service;
+
+import com.example.propertyrental.dto.PropertyRequestDto;
+import com.example.propertyrental.dto.PropertyResponseDto;
+import com.example.propertyrental.mapper.PropertyMapper;
+import com.example.propertyrental.model.Property;
+import com.example.propertyrental.model.Status;
+import com.example.propertyrental.model.Submission;
+import com.example.propertyrental.model.User;
+import com.example.propertyrental.repository.PropertyRepositoriy;
+import com.example.propertyrental.repository.SubmissionRepository;
+import com.example.propertyrental.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+@Transactional
+public class PropertyService {
+
+    private PropertyRepositoriy propertyRepositoriy;
+    private PropertyMapper propertyMapper;
+    private UserRepository userRepository;
+    private SubmissionRepository submissionRepository;
+
+
+    public Page<PropertyResponseDto> getAllProperties(int page, int size) {
+        Page<Property> pageProperties = propertyRepositoriy.findAllByStatus(PageRequest.of(page, size));
+        return pageProperties.map(propertyMapper::toPropertyResponseDto);
+    }
+
+    public PropertyResponseDto getProperty(UUID id) {
+        Property property = propertyRepositoriy.getById(id);
+        return propertyMapper.toPropertyResponseDto(property);
+    }
+
+
+    public void createProperty(PropertyRequestDto propertyRequestDto, String username) {
+        User user = userRepository.findUserByUserName(username);
+        Property property = propertyMapper.toProperty(propertyRequestDto, user);
+        Submission submission = new Submission();
+        submission.setProperty(property);
+        submission.setUser(user);
+        submission.setStatus(Status.PENDING);
+        submission.setCreated(LocalDate.now());
+        propertyRepositoriy.save(property);
+        submissionRepository.save(submission);
+    }
+}
