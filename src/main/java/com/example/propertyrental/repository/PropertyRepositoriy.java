f3af47b24ed0653604b703c314020af49b7849a7
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-05-27 10:54:09 +0200

    add method findAllByStatus

--- src/main/java/com/example/propertyrental/repository/PropertyRepositoriy.java
+++ src/main/java/com/example/propertyrental/repository/PropertyRepositoriy.java
@@ -2,11 +2,16 @@ package com.example.propertyrental.repository;
 
 
 import com.example.propertyrental.model.Property;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface PropertyRepositoriy extends JpaRepository<Property, UUID> {
+    @Query("select p from Property p where p not in(select s.property from Submission s where s.status='PENDING' )")
+    Page<Property> findAllByStatus(Pageable pageable);
 }
