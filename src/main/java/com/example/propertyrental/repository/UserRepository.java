95b6ffeb6647341c951f13ec4e7a4ea8b5c2bbc6
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-06-01 10:14:46 +0200

    add method findUser return optional

--- src/main/java/com/example/propertyrental/repository/UserRepository.java
+++ src/main/java/com/example/propertyrental/repository/UserRepository.java
@@ -4,12 +4,14 @@ import com.example.propertyrental.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    User findUserByUserName(String username);
+    Optional<User> findUserByUserName(String username);
 
     boolean existsByEmail(String email);
+
 }
