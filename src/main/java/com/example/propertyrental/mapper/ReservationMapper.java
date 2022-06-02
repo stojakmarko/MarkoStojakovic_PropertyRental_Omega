ab145534484feb9354b25c8e82ef41a4f55f908b
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-06-02 13:53:12 +0200

    add component annotation'

--- src/main/java/com/example/propertyrental/mapper/ReservationMapper.java
+++ src/main/java/com/example/propertyrental/mapper/ReservationMapper.java
@@ -1,21 +1,24 @@
 package com.example.propertyrental.mapper;
 
-import com.example.propertyrental.dto.ReservationDto;
 import com.example.propertyrental.model.Property;
 import com.example.propertyrental.model.Reservation;
 import com.example.propertyrental.model.User;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Data
 @Builder
+@Component
 public class ReservationMapper {
 
-    public Reservation toReservation(Property property, User user, ReservationDto reservationDto) {
+    public Reservation toReservation(Property property, User user, LocalDate fromDate, LocalDate toDate) {
         return Reservation.builder()
                 .property(property)
-                .reservationTo(reservationDto.toDate())
-                .reservationFrom(reservationDto.fromDate())
+                .reservationTo(toDate)
+                .reservationFrom(fromDate)
                 .user(user)
                 .build();
     }
--- src/main/java/com/example/propertyrental/mapper/SubmissionMapper.java
+++ src/main/java/com/example/propertyrental/mapper/SubmissionMapper.java
@@ -6,11 +6,13 @@ import com.example.propertyrental.model.Submission;
 import com.example.propertyrental.model.User;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
 @Data
 @Builder
+@Component
 public class SubmissionMapper {
 
     public Submission toSubmission(Property property, User user) {
