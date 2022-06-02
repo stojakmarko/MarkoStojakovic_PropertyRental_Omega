5eac8468f2a4ac90e1f2774b1894b90e6f1220c8
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-06-02 12:47:16 +0200

    add method search user with password token

--- src/main/java/com/example/propertyrental/repository/UserRepository.java
+++ src/main/java/com/example/propertyrental/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.propertyrental.repository;
 
 import com.example.propertyrental.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,7 +14,11 @@ public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByUserName(String username);
 
+    @Query(nativeQuery = true, value = "SELECT * FROM USERS WHERE PASSWORD_RESET_TOKEN=:token")
+    Optional<User> userWithPasswordResetToken(@Param("token") String token);
+
     boolean existsByEmail(String email);
+
     boolean existsByUserName(String username);
 
 }
