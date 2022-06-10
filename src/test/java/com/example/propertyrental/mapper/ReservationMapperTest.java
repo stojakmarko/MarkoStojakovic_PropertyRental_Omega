65d2906cb6624b3ac420b6a7b47714da0edea6ca
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-06-10 13:33:45 +0200

    PR-31 unit test reservation mapper

---
+++ src/test/java/com/example/propertyrental/mapper/ReservationMapperTest.java
@@ -0,0 +1,89 @@
+package com.example.propertyrental.mapper;
+
+import com.example.propertyrental.dto.ReservationResponseDto;
+import com.example.propertyrental.model.Property;
+import com.example.propertyrental.model.Reservation;
+import com.example.propertyrental.model.User;
+import com.example.propertyrental.model.UserRole;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ReservationMapperTest {
+
+
+    @Autowired
+    private ReservationMapper reservationMapper;
+
+    private Reservation reservation;
+    private Property property;
+    private User user;
+    private LocalDate reservationFrom;
+    private LocalDate reservationTo;
+
+    @BeforeEach
+    public void setUp() {
+        user = User
+                .builder()
+                .firstName("test")
+                .lastName("test")
+                .userName("test")
+                .email("test")
+                .userRole(UserRole.ROLE_CLIENT)
+                .build();
+
+        property = Property.builder()
+                .name("property")
+                .location("test")
+                .price(1000)
+                .availability(true)
+                .freeParking(true)
+                .numOfBedrooms(4)
+                .numOfSleepPlace(2)
+                .pool(false)
+                .wifi(true)
+                .owner(user)
+                .build();
+
+        reservationFrom = LocalDate.of(2022, 06, 10);
+        reservationTo = LocalDate.of(2022, 06, 17);
+
+        reservation = Reservation.builder()
+                .property(property)
+                .reservationTo(reservationTo)
+                .reservationFrom(reservationFrom)
+                .user(user)
+                .build();
+
+    }
+
+    @Test
+    void createReservationEntity_allFieldOk() {
+
+        Reservation reservation1 = reservationMapper.toReservation(property, user, reservationFrom, reservationTo);
+
+        assertEquals(Reservation.class, reservation1.getClass());
+        assertEquals(user, reservation1.getUser());
+        assertEquals(reservationFrom, reservation1.getReservationFrom());
+        assertEquals(reservationTo, reservation1.getReservationTo());
+
+    }
+
+    @Test
+    void createReservationResponseDto_allFieldsOk() {
+
+        ReservationResponseDto reservationResponseDto = reservationMapper.reservationResponseDto(reservation);
+
+        assertEquals(ReservationResponseDto.class, reservationResponseDto.getClass());
+        assertEquals(reservation.getUser().getId(), reservationResponseDto.getUserId());
+        assertEquals(reservation.getProperty().getId(), reservationResponseDto.getPropertyId());
+
+    }
+
+}
