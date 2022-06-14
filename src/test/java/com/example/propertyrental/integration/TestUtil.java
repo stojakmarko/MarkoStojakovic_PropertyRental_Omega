0614340c4520132b1193e36f9144e81b8d1f58ae
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-06-14 15:07:01 +0200

    add object mapper

---
+++ src/test/java/com/example/propertyrental/integration/TestUtil.java
@@ -0,0 +1,16 @@
+package com.example.propertyrental.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class TestUtil {
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
