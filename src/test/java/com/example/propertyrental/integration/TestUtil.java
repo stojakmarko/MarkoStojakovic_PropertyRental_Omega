585b08dc0acc11dbfbfba89a3f43f0739eeb0bb7
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-06-14 17:03:58 +0200

    add java time module

--- src/test/java/com/example/propertyrental/integration/TestUtil.java
+++ src/test/java/com/example/propertyrental/integration/TestUtil.java
@@ -1,12 +1,17 @@
 package com.example.propertyrental.integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class TestUtil {
 
     public static String asJsonString(final Object obj) {
         try {
-            return new ObjectMapper().writeValueAsString(obj);
+            ObjectMapper mapper = JsonMapper.builder()
+                    .addModule(new JavaTimeModule())
+                    .build();
+            return mapper.writeValueAsString(obj);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
