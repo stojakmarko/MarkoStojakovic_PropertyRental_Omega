6edc2825ae0f582de2b036054751f4a2d443ad8c
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-06-15 11:52:47 +0200

    add new method

--- src/test/java/com/example/propertyrental/integration/TestUtil.java
+++ src/test/java/com/example/propertyrental/integration/TestUtil.java
@@ -1,5 +1,7 @@
 package com.example.propertyrental.integration;
 
+import com.example.propertyrental.dto.PageResponseDto;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -17,5 +19,30 @@ public class TestUtil {
         }
     }
 
+    public static PageResponseDto<?> asPage(String json, TypeReference<?> typeReference) {
+        try {
+            ObjectMapper mapper = JsonMapper.builder()
+                    .addModule(new JavaTimeModule())
+                    .build();
+
+            return (PageResponseDto<?>) mapper.readValue(json, typeReference);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static Object asObject(String json, Class<?> type) {
+
+        try {
+            ObjectMapper mapper = JsonMapper.builder()
+                    .addModule(new JavaTimeModule())
+                    .build();
+            return mapper.readValue(json, type);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
