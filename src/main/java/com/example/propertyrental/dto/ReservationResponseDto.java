a4880ee1144a1c1394cca7fa586a57df53b1ad89
Marko Stojakovic <marko.stojakovic@vegait.rs>
2022-06-17 16:12:20 +0200

    add constructors

--- src/main/java/com/example/propertyrental/dto/ReservationResponseDto.java
+++ src/main/java/com/example/propertyrental/dto/ReservationResponseDto.java
@@ -1,14 +1,18 @@
 package com.example.propertyrental.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReservationResponseDto {
 
     private UUID id;
--- src/main/java/com/example/propertyrental/dto/SubmissionDto.java
+++ src/main/java/com/example/propertyrental/dto/SubmissionDto.java
@@ -1,14 +1,18 @@
 package com.example.propertyrental.dto;
 
 import com.example.propertyrental.model.Status;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class SubmissionDto {
 
     private UUID id;
